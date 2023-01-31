ecbede25f935c6b5c12acad4a63b16fb846dcbd8
Michelle Miller <105824981+MikiMiller95@users.noreply.github.com>
2023-01-31 11:03:11 -0600

    Add files via upload

---
+++ CIFARModelsArchitectures/DivisiveNorm/DivisiveNormalization.py
@@ -0,0 +1,81 @@
+from __future__ import division
+import torch
+import numbers
+import warnings
+import math
+from torch.nn.parameter import Parameter
+from torch.nn import Module
+from torch.nn import functional as F
+from torch._C import _infer_size, _add_docstr
+from torch.nn import _reduction as _Reduction
+from torch.nn.modules import utils
+from torch.nn.modules.utils import _single, _pair, _triple, _list_with_default
+from torch.nn import grad
+#from torch.nn import _VF
+from torch._jit_internal import boolean_dispatch, List
+from torch import __init__
+from torch.nn import __init__
+from torch.nn.modules._functions import CrossMapLRN2d as _cross_map_lrn2d
+from torch._C import *
+from itertools import repeat
+import torch.onnx.symbolic_helper as sym_help
+from torch.onnx.symbolic_helper import parse_args
+import torchvision
+
+class DivisiveNorm(Module):
+    r"""Applies divisive normalization along the feature dimension with an exponential kernel
+
+    math: b_c(x)=\frac{a_c(x)^2}{\left(k\left(1+\frac{\alpha}{\lambda} \sum_{j=-4 \lambda}^{4 \lambda} a_{c+j}(x)^2 e^{-|j| / \lambda}\right)\right)^\beta}
+
+    """
+    def __init__(self,lamb=5.,alpha=1., beta=0.75, k=1.):
+        super(DivisiveNorm, self).__init__()
+        self.lamb = Parameter(torch.Tensor([lamb]))
+        self.alpha = Parameter(torch.Tensor([alpha])) 
+        self.beta = Parameter(torch.Tensor([beta])) 
+        self.k = Parameter(torch.Tensor([k]))
+
+    def forward(self, input):
+        return divisive_normalization(input,self.lamb,self.alpha, self.beta, self.k)
+
+    def extra_repr(self):
+        return 'lambda={lamb},alpha={alpha}, beta={beta}, k={k}'.format(**self.__dict__['_parameters'])
+
+
+def divisive_normalization(input, lamb=5,  alpha=5, beta=0.75, k=1.):
+    """
+    Applies normalization across channels with exponential kernel
+    """
+    neighbors=int(torch.ceil(2*4*lamb).item())
+    if neighbors%2==0:
+        neighbors=neighbors+1
+    else:
+        pass
+    dim = input.dim()
+    if dim < 3:
+        raise ValueError('Expected 3D or higher dimensionality \
+                         input (got {} dimensions)'.format(dim))
+    div = input.mul(input).unsqueeze(1)
+    #hacky trick to try and keep everything on cuda
+    sizes = input.size()
+    weits = input.clone().detach() 
+    weits = weits.new_zeros(([1]+[1]+[int(neighbors)]+[1]+[1]))
+
+    if dim == 3:
+        div = F.pad(div, (0, 0, neighbors // 2,  neighbors - 1 // 2))
+        div = torch._C._nn.avg_pool2d((div,  neighbors, 1), stride=1).squeeze(1)
+    else:
+        dev = input.get_device()
+        # indexx is a 1D tensor that is a symmetric exponential distribution of some "radius" neighbors
+        idxs = torch.abs(torch.arange(neighbors)-neighbors//2)
+        weits[0,0,:,0,0]=idxs
+        weits= torch.exp(-weits/lamb)
+        # creating single dimension at 1;corresponds to number of input channels;only 1 input channel
+        # 3D convolution; weits has dims: Cx1xCx1x1 ; this means we have C filters for the C channels
+        # The div is the input**2; it has dimensions B x 1 x C x W x H
+        div=F.conv3d(div,weits,padding=((neighbors//2),0,0))
+        div=div/lamb
+
+    div = div.mul(alpha).add(1).mul(k).pow(beta)
+    div=div.squeeze()
+    return input.mul(input) / div
---
+++ CIFARModelsArchitectures/PytorchNormModels/Batch.py
@@ -0,0 +1,95 @@
+import os
+import torch
+import torch.nn as nn
+import tqdm
+import numpy as np
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils import data
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+class Batch(nn.Module):
+    """
+    AlexNet model with Batch Normalization
+    """
+    def __init__(self, num_classes=100):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1,padding=1),  # (b x 96 x 54 x 54)
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 96 x 27 x 27)
+            nn.Conv2d(64, 192, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.BatchNorm2d(192), 
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 13 x 13)
+            nn.Conv2d(192, 384, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.BatchNorm2d(384), 
+            nn.Conv2d(384, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.BatchNorm2d(256), 
+            nn.Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1)),
+            nn.ReLU(),
+            nn.BatchNorm2d(256), 
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 6 x 6)
+        )
+        # classifier is just a name for linear layers
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            #nn.utils.weight_norm(nn.Linear(256 * 6 * 6, 4096), name='weight',dim=None),
+            nn.ReLU(inplace=False),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            #nn.utils.weight_norm(nn.Linear(4096, 4096), name='weight',dim=None),
+            nn.ReLU(inplace=False),
+            nn.Linear(4096, num_classes),
+            #nn.utils.weight_norm(nn.Linear(4096, num_classes), name='weight',dim=None),
+        )
+    def init_weights(m):
+        """initialize weight of Linear layer as constant_weight"""
+        if type(m) == nn.Conv2d:
+            nn.init.kaiming_normal_(m, a=0, mode='fan_in', nonlinearity='relu')
+        #    torch.nn.init.calculate_gain('ReLU', param=None)
+        if type(m) == nn.Linear:
+            nn.init.kaiming_normal_(m, a=0, mode='fan_in', nonlinearity='relu')
+            #torch.nn.init.calculate_gain('ReLU', param=None)
+
+    def init_bias(self):
+        for layer in self.features:
+            if isinstance(layer, nn.Conv2d):
+                nn.init.normal_(layer.weight, mean=0, std=0.01)
+                nn.init.constant_(layer.bias, 0)
+            if isinstance(layer, LearnLocalResponseNorm):
+                print("INSIDE INSTANCE OF LEARN")
+                nn.init.constant_(layer.size, 5)
+                nn.init.constant_(layer.alpha, [1.])
+                nn.init.constant_(layer.beta, .75)
+                nn.init.constant_(layer.k, 1.)
+        # original paper = 1 for Conv2d layers 2nd, 4th, and 5th conv layers
+        nn.init.constant_(self.features[3].bias, 1)
+        nn.init.constant_(self.features[8].bias, 1)
+        nn.init.constant_(self.features[10].bias, 1)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+    
+def alexnet(pretrained=False, progress=True, **kwargs):
+    r"""AlexNet model architecture from the
+        pretrained (bool): If True, returns a model pre-trained on ImageNet
+        progress (bool): If True, displays a progress bar of the download to stderr
+    """
+    model = AlexNet(**kwargs)
+    #model.apply(init_weights)
+    if pretrained:
+        state_dict = load_state_dict_from_url(model_urls['alexnet'],
+                                              progress=progress)
+        model.load_state_dict(state_dict)
+    return model
